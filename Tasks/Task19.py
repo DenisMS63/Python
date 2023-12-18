3e71d12cb346b906d13ca89ef62577b0ec3bc99b
Denis <moiseev-denis@mail.ru>
2023-12-18 15:54:53 +0400

    Решил задачу 19, второй способ

--- Tasks/Task19.py
+++ Tasks/Task19.py
@@ -10,11 +10,17 @@ list_1 = [i for i in range(1, randint(7, 10))]
 print(list_1)
 n = int(input("Введите кол-во сдвигов: "))
 
-for i in range(n):
-    last_num = list_1.pop()
-    list_1.insert(0, last_num)
+# for i in range(n):
+#     last_num = list_1.pop()
+#     list_1.insert(0, last_num)
 
-print(list_1)
+# print(list_1)
 
 # Срезы
-print(list_1[-n:] + list_1[:-n])
+# print(list_1[-n:] + list_1[:-n])
+
+print(list_1[-n:])
+print(list_1[:-n])
+print()
+print(list_1[n:])
+print(list_1[:n])
