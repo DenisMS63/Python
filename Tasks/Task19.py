74394ebf410e6562d0ba6a3a4e023d39d5f6a99c
Denis <moiseev-denis@mail.ru>
2023-12-18 15:42:07 +0400

    Решил задачу 19, первый способ

---
+++ Tasks/Task19.py
@@ -0,0 +1,17 @@
+# Дана последовательность из N целых чисел и число К.
+# Необходимо сдвинуть всю последовательность (сдвиг - циклический)
+# на К элементов вправо, К - положительное число.
+# [1, 2, 3, 4, 5] k = 3
+# [3, 4, 5, 1, 2]
+
+from random import randint
+
+list_1 = [i for i in range(1, randint(7, 10))]
+print(list_1)
+n = int(input("Введите кол-во сдвигов: "))
+
+for i in range(n):
+    last_num = list_1.pop()
+    list_1.insert(0, last_num)
+
+print(list_1)
