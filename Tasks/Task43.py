bca3c492906954d6e47793de3c16b42b653f6411
Denis <moiseev-denis@mail.ru>
2023-12-27 00:17:48 +0400

    Решил задачу 43

--- Tasks/Task43.py
+++ Tasks/Task43.py
@@ -7,15 +7,25 @@
 
 from random import randint
 
+# size = int(input("Введите размер списка: "))
+# list_1 = [randint(1, 5) for i in range(size)]
+
+# print(list_1)
+# count = 0
+# for i in range(size - 1):
+#     for j in range(i + 1, size):
+#         if list_1[i] == list_1[j]:
+#             count += 1        
+
+# print(count)
+
 size = int(input("Введите размер списка: "))
 list_1 = [randint(1, 5) for i in range(size)]
 
 print(list_1)
+
 count = 0
 for i in range(size - 1):
-    for j in range(i + 1, size):
-        if list_1[i] == list_1[j]:
-            count += 1
-        
+    count += list_1[i + 1:].count(list_1[i])
 
 print(count)
