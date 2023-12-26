3180a73be5c1aa163c711576b49da71e05d99002
Denis <moiseev-denis@mail.ru>
2023-12-27 00:04:06 +0400

    Решил задачу 43

--- Tasks/Task41.py
+++ Tasks/Task41.py
@@ -14,8 +14,7 @@ list_1 = [randint(1, 10) for i in range(size)]
 print(list_1)
 
 count = 0
-for i in range(1, len(list_1) - 1): 
-     
+for i in range(1, len(list_1) - 1):      
     if list_1[i - 1] < list_1[i] > list_1[i + 1]:
         count += 1
 print(count)
