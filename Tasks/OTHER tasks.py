4e244ff3885bb0313a6c2cc20d54e713b5d24e5b
Denis <moiseev-denis@mail.ru>
2023-12-29 14:36:24 +0400

    Задача из лекции 4 решение преподавателя

--- Tasks/OTHER tasks.py
+++ Tasks/OTHER tasks.py
@@ -1,15 +1,20 @@
 # В списке хранятся числа. Нужно выбрать только четные числа
 # и составить список пар (число; квадрат числа) 
 
-
-
-
-
 list1 = [1, 2, 3, 5, 8, 15, 23, 38]
 
-for el in list1:
-    if el % 2 == 0:
-        print(el)
+# for el in list1:
+#     if el % 2 == 0:
+#         print(el)
 
-list2 = [f"{el}; {el ** 2}" for el in list1 if el % 2 == 0]
-print(list2)
+# list2 = [f"{el}; {el ** 2}" for el in list1 if el % 2 == 0]
+# print(list2)
+
+# Решение преподавателя
+
+res = list()
+
+for i in list1:
+    if i % 2 == 0:
+        res.append((i, i ** 2))
+print(res)
