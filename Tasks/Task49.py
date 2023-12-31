bd211e3a5fc54e96a3a015b535b3daa6d271d9b5
Denis <moiseev-denis@mail.ru>
2023-12-31 04:57:54 +0400

    Решение 2 задачи 49

--- Tasks/Task49.py
+++ Tasks/Task49.py
@@ -23,18 +23,30 @@
 # 2.5 10
 
 
-s = lambda cur_tuple: cur_tuple[0] * cur_tuple[1]
-orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-# print(*find_farthest_orbit(orbits))
-max_orb = s(orbits[0])
-index = 0
+# s = lambda cur_tuple: cur_tuple[0] * cur_tuple[1]
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# # print(*find_farthest_orbit(orbits))
+# max_orb = s(orbits[0])
+# find_tuple = orbits[0]
 
-for cur_orb in orbits[1:]:
-    if cur_orb[0] != cur_orb[1]:
-        cur_s = s(cur_orb)
-        if cur_s > max_orb:
-            max_orb = cur_s
-            index = cur_orb
-print(max_orb)
-print(index)
+# for cur_orb in orbits[1:]:
+#     if cur_orb[0] != cur_orb[1]:
+#         cur_s = s(cur_orb)
+#         if cur_s > max_orb:
+#             max_orb = cur_s
+#             index = cur_orb
+# print(max_orb)
+# print(index)
     
+# РЕШЕНИЕ 2
+
+def find_farthest_orbit(list_of_orbits):
+    filter_orbs = list(filter(lambda cur_orb: cur_orb[0] != cur_orb[1], list_of_orbits))
+    tuple_s = list(map(lambda cur_orb: cur_orb[0] * cur_orb[1], filter_orbs))
+    max_s = max(tuple_s)
+    i_max_s = tuple_s.index(max_s)
+    return  filter_orbs[i_max_s]
+
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+print(find_farthest_orbit(orbits))
